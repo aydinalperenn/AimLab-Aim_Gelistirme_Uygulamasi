f0906d53152f016b517ba40ded380164cbe0e22b
Alperen Aydin <aa.aydinalperen@gmail.com>
2023-02-05 19:59:31 +0300

    Add project files.

---
+++ Aim Geliştirme Uygulaması/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace Aim_Geliştirme_Uygulaması
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSkor = new System.Windows.Forms.Label();
+            this.lblSure = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblKalanHak = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(159)))), ((int)(((byte)(159)))));
+            this.pictureBox1.Location = new System.Drawing.Point(375, 225);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(159)))), ((int)(((byte)(159)))));
+            this.label1.Location = new System.Drawing.Point(651, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Skor:";
+            // 
+            // lblSkor
+            // 
+            this.lblSkor.AutoSize = true;
+            this.lblSkor.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSkor.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(159)))), ((int)(((byte)(159)))));
+            this.lblSkor.Location = new System.Drawing.Point(726, 18);
+            this.lblSkor.Name = "lblSkor";
+            this.lblSkor.Size = new System.Drawing.Size(31, 31);
+            this.lblSkor.TabIndex = 2;
+            this.lblSkor.Text = "0";
+            // 
+            // lblSure
+            // 
+            this.lblSure.AutoSize = true;
+            this.lblSure.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSure.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(159)))), ((int)(((byte)(159)))));
+            this.lblSure.Location = new System.Drawing.Point(98, 18);
+            this.lblSure.Name = "lblSure";
+            this.lblSure.Size = new System.Drawing.Size(31, 31);
+            this.lblSure.TabIndex = 4;
+            this.lblSure.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(159)))), ((int)(((byte)(159)))));
+            this.label4.Location = new System.Drawing.Point(25, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 31);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Süre:";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // lblKalanHak
+            // 
+            this.lblKalanHak.AutoSize = true;
+            this.lblKalanHak.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKalanHak.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(159)))), ((int)(((byte)(159)))));
+            this.lblKalanHak.Location = new System.Drawing.Point(454, 18);
+            this.lblKalanHak.Name = "lblKalanHak";
+            this.lblKalanHak.Size = new System.Drawing.Size(31, 31);
+            this.lblKalanHak.TabIndex = 6;
+            this.lblKalanHak.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(159)))), ((int)(((byte)(159)))));
+            this.label3.Location = new System.Drawing.Point(308, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 31);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Kalan Hak:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(159)))), ((int)(((byte)(159)))));
+            this.button1.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.button1.Location = new System.Drawing.Point(338, 212);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 96);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Başla";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(-1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(801, 500);
+            this.panel1.TabIndex = 8;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblKalanHak);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblSure);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblSkor);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Aim Geliştirme Uygulaması";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSkor;
+        private System.Windows.Forms.Label lblSure;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblKalanHak;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Aim Geliştirme Uygulaması/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aim_Geliştirme_Uygulaması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        int sure = 60;
+        int skor = 0;
+        int can = 3;
+
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+            Random rnd = new Random();
+
+            skor++;
+
+            lblSkor.Text = skor.ToString();
+
+            int nokta_x;
+            nokta_x = rnd.Next(0, 700);
+            pictureBox1.Location = new Point(nokta_x, pictureBox1.Location.Y);
+
+            int nokta_y;
+            nokta_y = rnd.Next(70, 400);
+            pictureBox1.Location = new Point(pictureBox1.Location.X, nokta_y);
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            sure--;
+            lblSure.Text = sure.ToString();
+
+            if (sure == 0)
+            {
+                timer1.Stop();
+                MessageBox.Show("Süre Doldu!\nSkorunuz: " + skor.ToString(), "Oyun Bitti", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Application.Restart();
+            }
+
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblSure.Text = "60";
+            timer1.Start();
+            pictureBox1.Visible = true;
+            lblKalanHak.Text = "3";
+            panel1.Enabled = true;
+            this.Controls.Remove(button1);
+            button1.Visible = false;
+        }
+
+        private void panel1_MouseDown_1(object sender, MouseEventArgs e)
+        {
+            can--;
+            lblKalanHak.Text = can.ToString();
+            if (can == 0)
+            {
+                timer1.Stop();
+                MessageBox.Show("Kaybettiniz!\nSkorunuz: " + skor.ToString(), "Oyun Bitti", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Application.Restart();
+            }
+        }
+    }
+}
